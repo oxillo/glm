9f87b698ca62b2a7fb942a9db58ca4cea5cb6cd7
oxillo <oxillo@hotmail.com>
2019-12-07 06:11:50 +0100

    static cast to void*

--- test/core/core_cpp_defaulted_ctor.cpp
+++ test/core/core_cpp_defaulted_ctor.cpp
@@ -16,28 +16,28 @@ static int test_vec_memcpy()
 	{
 		glm::ivec1 const A = glm::ivec1(76);
 		glm::ivec1 B;
-		std::memcpy(&B, &A, sizeof(glm::ivec1));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::ivec1));
 		Error += B == A ? 0 : 1;
 	}
 
 	{
 		glm::ivec2 const A = glm::ivec2(76);
 		glm::ivec2 B;
-		std::memcpy(&B, &A, sizeof(glm::ivec2));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::ivec2));
 		Error += B == A ? 0 : 1;
 	}
 
 	{
 		glm::ivec3 const A = glm::ivec3(76);
 		glm::ivec3 B;
-		std::memcpy(&B, &A, sizeof(glm::ivec3));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::ivec3));
 		Error += B == A ? 0 : 1;
 	}
 
 	{
 		glm::ivec4 const A = glm::ivec4(76);
 		glm::ivec4 B;
-		std::memcpy(&B, &A, sizeof(glm::ivec4));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::ivec4));
 		Error += B == A ? 0 : 1;
 	}
 
@@ -51,63 +51,63 @@ static int test_mat_memcpy()
 	{
 		glm::mat2x2 const A = glm::mat2x2(76);
 		glm::mat2x2 B;
-		std::memcpy(&B, &A, sizeof(glm::mat2x2));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat2x2));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat2x3 const A = glm::mat2x3(76);
 		glm::mat2x3 B;
-		std::memcpy(&B, &A, sizeof(glm::mat2x3));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat2x3));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat2x4 const A = glm::mat2x4(76);
 		glm::mat2x4 B;
-		std::memcpy(&B, &A, sizeof(glm::mat2x4));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat2x4));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat3x2 const A = glm::mat3x2(76);
 		glm::mat3x2 B;
-		std::memcpy(&B, &A, sizeof(glm::mat3x2));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat3x2));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat3x3 const A = glm::mat3x3(76);
 		glm::mat3x3 B;
-		std::memcpy(&B, &A, sizeof(glm::mat3x3));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat3x3));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat3x4 const A = glm::mat3x4(76);
 		glm::mat3x4 B;
-		std::memcpy(&B, &A, sizeof(glm::mat3x4));
+		std::memcpystatic_cast<void*>(B), &A, sizeof(glm::mat3x4));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat4x2 const A = glm::mat4x2(76);
 		glm::mat4x2 B;
-		std::memcpy(&B, &A, sizeof(glm::mat4x2));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat4x2));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat4x3 const A = glm::mat4x3(76);
 		glm::mat4x3 B;
-		std::memcpy(&B, &A, sizeof(glm::mat4x3));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat4x3));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
 	{
 		glm::mat4x4 const A = glm::mat4x4(76);
 		glm::mat4x4 B;
-		std::memcpy(&B, &A, sizeof(glm::mat4x4));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::mat4x4));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
@@ -121,7 +121,7 @@ static int test_quat_memcpy()
 	{
 		glm::quat const A = glm::quat(1, 0, 0, 0);
 		glm::quat B;
-		std::memcpy(&B, &A, sizeof(glm::quat));
+		std::memcpy(static_cast<void*>(&B), &A, sizeof(glm::quat));
 		Error += glm::all(glm::equal(B, A, glm::epsilon<float>())) ? 0 : 1;
 	}
 
